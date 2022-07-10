cee8a0379a7491a3fae714ebc5474c7da9284bea
db360 <da.b@hotmail.es>
2022-07-10 12:54:50 +0200

    cambios social media

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs'
 import { FaFacebookF } from 'react-icons/fa'
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter />
+        <a href='https://www.instagram.com/dabeat360/' target="_blank" rel="noreferrer">
+          <BsInstagram />
+        </a>
         </div>
         <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
+        <a href='https://github.com/db360/' target="_blank" rel="noreferrer">
+          <BsGithub />
+        </a>
         </div>
     </div>
   )
